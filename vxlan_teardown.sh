d1d09edf0f586cfdb6f3a34f0594e4cb7b618c09
enricobrum <e.brunelli010@studenti.unibs.it>
2025-06-19 08:59:08 +0200

    Modifica programmi client server

--- vxlan_teardown.sh
+++ vxlan_teardown.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 # Verifica numero di argomenti
-if [ "$#" -ne 1 ]; then
-    echo "Uso: $0 <VXLAN_ID>"
-    echo "Esempio: $0 42"
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <VXLAN_ID> $1 <BR_ID>
+    echo "Esempio: $0 42 $1 br42"
+
     exit 1
 fi
 
 VXLAN_ID=$1
+BR_ID=$2
+BRIDGE_IF="br${BR_ID}"
 VXLAN_IF="vxlan${VXLAN_ID}"
 
 echo "Rimozione dell'interfaccia $VXLAN_IF..."
-sudo ip link delete $VXLAN_IF 2>/dev/null
 
+sudo ip link delete $VXLAN_IF 2>/dev/null
+sudo ip link delete 
 if [ $? -eq 0 ]; then
     echo "Interfaccia $VXLAN_IF rimossa con successo."
 else
     echo "Errore: impossibile rimuovere $VXLAN_IF (forse non esiste)."
 fi
+
+echo "Rimozione del bridge $BRIDGE_IF..."
+sudo ip link delete $BRIDGE_IF 2>/dev/null
+if [ $? -eq 0 ]; then
+    echo "Bridge $BRIDGE_IF rimosso con successo."
+else
+    echo "Errore: impossibile rimuovere $BRIDGE_IF (forse non esiste)."
+fi
+
